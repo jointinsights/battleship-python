eb790377bceb48dfd6a525f855e0f026bf29b132
Rich Visotcky <visotcky@gmail.com>
2026-08-18 15:14:43 -0500

    Fix TelemetryClient handler lock issue

--- torpydo/telemetryclient.py
+++ torpydo/telemetryclient.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 
 from opencensus.ext.azure.log_exporter import AzureEventHandler
 
@@ -6,9 +7,13 @@ logger = logging.getLogger(__name__)
 
 class TelemetryClient(object):
     def init():
-        logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=c764f176-19a5-4949-825d-9f30db2f14e8;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/'))
+        handler = AzureEventHandler(connection_string='InstrumentationKey=c764f176-19a5-4949-825d-9f30db2f14e8;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/')
+
+        if handler.lock is None:
+            handler.lock = threading.Lock()
+
+        logger.addHandler(handler)
         logger.setLevel(logging.INFO)
 
     def trackEvent(eventName: str, properties: object):
-        logger.info(eventName, extra=properties)        
-
+        logger.info(eventName, extra=properties)
